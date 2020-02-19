b4c2e9040e57c87a8a9f59e654cafbc976c2ee07
srijani157 <srijani.sarkar@winsomeitsolutions.com>
2020-02-19 17:44:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Select from "react-select";
+const api = {
+  key: "632f2aa05659f8386772f53444cf4aad",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
 
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" }
+];
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [option, setOption] = useState("");
+  const handleChange = option => {
+    console.log("abc");
+    setOption({ option });
+    console.log(`Option selected:`, option);
+  };
+  const search = evt => {
+    console.log("abcd");
+
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = d => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <Select
+            placeholder="Search..."
+            value={option}
+            onChange={handleChange}
+            options={options}
+          />
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">
+                {weather.weather[0].main}
+                {/* <br />
+                {weather.weather[0].description} */}
+              </div>
+              <div className="weather1">
+                feels like:{Math.round(weather.main.feels_like)}°c
+                <br />
+                min-temp:{Math.round(weather.main.temp_min)}°c
+                <br />
+                max-temp:{Math.round(weather.main.temp_max)}°c
+                <br />
+                humidity:{Math.round(weather.main.humidity)}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                <h1 style={{ color: "white" }}>not found</h1>
+              </div>
+            </div>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
