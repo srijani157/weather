4ea6ab7ced1aa721e55ff28c4a08ea345adcf512
srijani157 <srijani.sarkar@winsomeitsolutions.com>
2020-04-01 19:43:37 +0530

    'final'

--- src/App.js
+++ src/App.js
@@ -1,90 +1,43 @@
 import React, { useState } from "react";
-import Select from "react-select";
 const api = {
   key: "632f2aa05659f8386772f53444cf4aad",
   base: "https://api.openweathermap.org/data/2.5/"
 };
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" }
-];
-function App() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
-  const [option, setOption] = useState("");
-  const handleChange = option => {
-    console.log("abc");
-    setOption({ option });
-    console.log(`Option selected:`, option);
-  };
-  const search = evt => {
-    console.log("abcd");
 
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
-          setQuery("");
+          setQuery('');
           console.log(result);
         });
     }
-  };
+  }
 
-  const dateBuilder = d => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
-  };
+    return `${day} ${date} ${month} ${year}`
+  }
 
   return (
-    <div
-      className={
-        typeof weather.main != "undefined"
-          ? weather.main.temp > 16
-            ? "app warm"
-            : "app"
-          : "app"
-      }
-    >
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
-          <Select
-            placeholder="Search..."
-            value={option}
-            onChange={handleChange}
-            options={options}
-          />
-          <input
+          <input 
             type="text"
             className="search-bar"
             placeholder="Search..."
